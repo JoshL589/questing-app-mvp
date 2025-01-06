806222a7717f1b18bd54c1b0c1fef0a44fe2ab2a
Joshua Li <joshuazli2020@email.com>
2025-01-06 17:02:06 +1000

    adding comments for the functions that need implementing

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("script.js connected")
+
+//start and stop timer
+
+//changing the timer length in settings with a popup
+
+//when timer finishes, submit quest name and work description to mongodb
+
+//when clicking completed quests, fetching all completed quests from mongodb
